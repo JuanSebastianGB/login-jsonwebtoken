e0439482a79d84feb46e475568774b38460ff983
JuanSebastianGB <3393@holbertonschool.com>
2022-05-16 00:35:43 -0500

    Finishing Product CRUD

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -11,12 +11,18 @@ export const getProducts = async (req, res) => {
     const products = await Product.find();
     res.status(200).json({ products });
 }
-export const getProductById = (req, res) => {
-    res.send('Creating product');
+export const getProductById = async (req, res) => {
+    const { productId } = req.params;
+    const product = await Product.findById(productId);
+    res.status(200).json({ product });
 }
-export const updateProductById = (req, res) => {
-    res.send('Creating product');
+export const updateProductById = async (req, res) => {
+    const { productId } = req.params;
+    const product = await Product.findByIdAndUpdate(productId, req.body, { new: true });
+    res.status(200).json({ product });
 }
-export const deleteProductById = (req, res) => {
-    res.send('Creating product');
+export const deleteProductById = async (req, res) => {
+    const { productId } = req.params;
+    const product = await Product.findByIdAndDelete(productId);
+    res.status(200).json({ success: 'deleted', product });
 }
